b4bba950289c75cf9e0581280bf38c8228a6c3df
huhuiyu <1069306849@qq.com>
2023-09-11 08:33:21 +0800

    多仓库提交脚本

---
+++ push.ps1
@@ -0,0 +1,4 @@
+# github链接地址：git remote add github git@github.com:huhuiyu/java-project-2103.git
+# jihulab链接地址：git remote add jihulab git@jihulab.com:DarkKnight/java-project-2103.git
+# gitee链接地址：git remote add gitee git@gitee.com:huhuiyu/java-project-2103.git
+git push
