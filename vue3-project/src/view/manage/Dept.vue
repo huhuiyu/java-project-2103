5c51ecc13242635016efa2ef8d1b934e75f954e7
huhuiyu <1069306849@qq.com>
2023-09-25 11:44:08 +0800

    部门管理完成

--- vue3-project/src/view/manage/Dept.vue
+++ vue3-project/src/view/manage/Dept.vue
@@ -11,6 +11,7 @@
       <ElFormItem>
         <ElButton v-loading="loading" @click="requery" type="primary">查询</ElButton>
         <ElButton v-loading="loading" @click="reset" type="danger">重置</ElButton>
+        <ElButton @click="avisible = true" type="warning">添加</ElButton>
       </ElFormItem>
     </ElForm>
   </div>
@@ -26,22 +27,67 @@
       </ElTableColumn>
       <ElTableColumn label="操作">
         <template #default="scope">
-          <ElButton type="primary">修改</ElButton>
-          <ElButton type="danger">删除</ElButton>
+          <ElButton @click="showModify(scope.row)" type="primary">修改</ElButton>
+          <ElButton @click="del(scope.row)" type="danger">删除</ElButton>
         </template>
       </ElTableColumn>
     </ElTable>
 
     <ElPagination :total="page.total" v-model:page-size="page.pageSize" v-model:current-page="page.pageNumber" @current-change="query" @size-change="requery" :page-sizes="[3, 5, 10, 20]" layout="prev,pager,next,total,sizes"></ElPagination>
   </div>
+
+  <!-- 添加的对话框 -->
+  <ElDialog v-model="avisible" title="添加部门信息" width="30%" :close-on-click-modal="false" @closed="requery">
+    <div>
+      <ElForm v-loading="adding">
+        <ElFormItem>
+          <ElInput v-model="addInfo.deptName"></ElInput>
+        </ElFormItem>
+
+        <ElFormItem>
+          <ElInput v-model="addInfo.deptInfo"></ElInput>
+        </ElFormItem>
+      </ElForm>
+    </div>
+
+    <template #footer>
+      <div>
+        <ElButton @click="add" v-loading="adding" type="primary">添加</ElButton>
+        <ElButton @click="avisible = false" type="danger">关闭</ElButton>
+      </div>
+    </template>
+  </ElDialog>
+
+  <!-- 修改的对话框 -->
+  <ElDialog v-model="mvisible" title="修改部门信息" width="30%" :close-on-click-modal="false" @closed="requery">
+    <div>
+      <ElForm v-loading="modifying">
+        <ElFormItem>
+          <ElInput v-model="modifyInfo.deptName"></ElInput>
+        </ElFormItem>
+
+        <ElFormItem>
+          <ElInput v-model="modifyInfo.deptInfo"></ElInput>
+        </ElFormItem>
+      </ElForm>
+    </div>
+
+    <template #footer>
+      <div>
+        <ElButton @click="modify" v-loading="modifying" type="primary">保存</ElButton>
+        <ElButton @click="mvisible = false" type="danger">关闭</ElButton>
+      </div>
+    </template>
+  </ElDialog>
+  
 </template>
 <script lang="ts" setup>
 import { ref } from 'vue'
 import ApiService from '../../ts/ApiService'
 import Tools from '../../ts/Tools'
-import { BaseListResult, PageInfo } from '../../ts/BaseResult'
+import { BaseResult, BaseListResult, PageInfo } from '../../ts/BaseResult'
 import { Dept } from '../../ts/Manage'
-import { ElButton, ElMessageBox, ElPagination, ElTable, ElTableColumn } from 'element-plus'
+import { ElDialog, ElButton, ElMessageBox, ElPagination, ElTable, ElTableColumn, ElForm, ElFormItem, ElInput } from 'element-plus'
 
 const title = ref('部门管理')
 
@@ -75,11 +121,72 @@ function query() {
       list.value = data.list
       return
     }
-    ElMessageBox.alert(data.message, '部门管理')
   })
 }
 
 query()
+//#endregion
+
+//#region 添加的部分
+const addInfo = ref(new Dept())
+const adding = ref(false)
+const avisible = ref(false)
+
+function add() {
+  adding.value = true
+  ApiService.post('/manage/dept/add', addInfo.value, (data: BaseResult) => {
+    adding.value = false
+    if (data.success) {
+      ElMessageBox.alert(data.message, '部门添加')
+    }
+  })
+}
+
+//#endregion
+
+//#region 删除的部分
+
+function del(info: Dept) {
+  ElMessageBox.confirm(`是否删除：${info.deptName}？`)
+    .then(() => {
+      ApiService.post(
+        '/manage/dept/delete',
+        {
+          deptId: info.deptId,
+        },
+        (data: BaseResult) => {
+          if (data.success) {
+            ElMessageBox.alert(data.message)
+            requery()
+          }
+        },
+      )
+    })
+    .catch(() => {})
+}
+
+//#endregion
+
+//#region 修改的部分
+const modifyInfo = ref(new Dept())
+const modifying = ref(false)
+const mvisible = ref(false)
+
+function showModify(info: Dept) {
+  modifyInfo.value = Tools.concatJson(info)
+  mvisible.value = true
+}
+
+function modify() {
+  modifying.value = true
+  ApiService.post('/manage/dept/update', modifyInfo.value, (data: BaseResult) => {
+    modifying.value = false
+    if (data.success) {
+      ElMessageBox.alert(data.message)
+    }
+  })
+}
+
 //#endregion
 </script>
 <style></style>
